540634699a0382572f8e2440c5c3755c73a4712f
4nemfreddo <killmyenvy@gmail.com>
2025-03-09 00:54:12 +0300

    updated dotenv

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from 'express'
 
 const app = express();
 
-app.length('/', (req, res) => {
-    
-})
+app.get('/', (req, res) => {
+    res.send({body: 'Welcome to Sunscription API'});
+});
+
+app.listen(3000, () => {
+    console.log('Server Running on port 3000')
+});
+
+export default app;
