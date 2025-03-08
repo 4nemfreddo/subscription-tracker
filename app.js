0ac73f490ba1cbae31d8004c31db97a20793fb20
4nemfreddo <killmyenvy@gmail.com>
2025-03-09 01:12:06 +0300

    updated configs

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express'
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send({body: 'Welcome to Sunscription API'});
 });
 
-app.listen(3000, () => {
-    console.log('Server Running on port 3000')
+app.listen(PORT, () => {
+    console.log(`Server Running on http://localhost:${PORT}`);
 });
 
 export default app;
