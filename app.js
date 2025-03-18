512ddbb581ced1cb4feceffaea5f16b9468808cf
4nemfreddo <killmyenvy@gmail.com>
2025-03-19 00:00:06 +0300

    update database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import userRouter from './config/routes/user.routes.js';
 import authRouter from './config/routes/auth.routes.js';
 import subscriptionRouter from './config/routes/subscriptions.routes.js';
+import connectToDatabase from './config/database/mongodb.js';
 
 const app = express();
 
@@ -16,8 +17,11 @@ app.get('/', (req, res) => {
     res.send({body: 'Welcome to Sunscription API'});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server Running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
+
 });
 
 export default app;
