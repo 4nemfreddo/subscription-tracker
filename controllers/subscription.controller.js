1df11223a3a2f845423dfcc920d8310b4eb1e5a7
4nemfreddo <killmyenvy@gmail.com>
2025-06-08 21:37:34 +0300

    fix subscription controller feature, fix mongodb atlas auth error

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -14,4 +14,19 @@ export const createSubscription = async (req, res, next) => {
     }
 }
 
-export default createSubscription;
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        // Check if user is same as one in the token
+        if(req.user.id != req.params.id) {
+            const error = new Error('You are not the owner of the account');
+            error.status = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user: req.params.id});
+
+        res.status(200).json({ success:true, data: subscriptions });
+    } catch (e){
+        next(e);
+    }
+}
